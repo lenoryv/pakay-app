38fdb196d338e3b80b59c86489d503aa006613ed
davidLM20 <jhandry13@hotmail.com>
2022-07-27 18:26:42 -0500

    PA-12 variables de pantalla

--- src/app/UI/views/Booking/backup-interface/backup-interface.page.ts
+++ src/app/UI/views/Booking/backup-interface/backup-interface.page.ts
@@ -1,21 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Data, Router } from '@angular/router';
 
+export interface Booking{
+  id: string;
+  dateIn: Date;
+  dateOut: Date;
+  price: number;
+  numberGuest: number;
+  numberRooms: number;
+  idRoom: string;
+  idClient: string;
+}
+
+export interface Rooms{
+  id?: string;
+  description?: string;
+  type?:string;
+  price?: number;
+  minibar?: boolean;
+  photos?:string;
+  numBeds?: number;
+  stateCleanliness?: boolean;
+}
+
+export interface AuxBooking extends Omit<Booking,'idRoom'> {
+  room: Rooms;
+}
 @Component({
   selector: 'app-backup-interface',
   templateUrl: './backup-interface.page.html',
   styleUrls: ['./backup-interface.page.scss'],
 })
+
 export class BackupInterfacePage implements OnInit {
 
+  booking!: AuxBooking;
+  room!: Rooms;
+  auxBooking!: Booking;
+
+  iva = 0;
+  tmpAux = 0;
+
+  auxdateIn = new Date(2022,6,24);
+  auxdateOn = new Date();
+
   constructor(
-    private router: Router
+    private router: Router,
+    //private roomsService: roosMicroService,
   ) { }
 
+  fecthRoom(booking:Booking){
+    /*
+      this.roomsService.getRoomById(booking.idRoom).subscribe(
+        room =>{
+          Let aux: AuxBooking = {...booking,idRoom:room}
+          this.booking = aux
+        }
+      )
+    */
+  }
   goPayment(){
     this.router.navigate(['/confirm-payment'])
     console.log("********")
   }
+
   goBack(){
     this.router.navigate(['/login-view']);
   }
